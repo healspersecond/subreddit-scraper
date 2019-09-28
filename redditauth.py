62fdae5ad23479dfb99308ba5d4a531e65d508c8
healspersecond <ckiene@uw.edu>
2019-09-28 10:28:56 -0700

    Update redditauth.py

--- redditauth.py
+++ redditauth.py
@@ -1,8 +1,10 @@
 import praw
 
+# You'll encounter these steps in many guides on accessing Reddit's API, so I shouldn't have to explain much. 
+# Just remember to replace all of the strings here with their respective entries (e.g., YOUR-CLIENT-ID = your bot's Client ID).
 
-reddit = praw.Reddit(client_id='YOUR-ID', \
-                     client_secret='YOUR-SECRET', \
+reddit = praw.Reddit(client_id='YOUR-CLIENT-ID', \
+                     client_secret='YOUR-CLIENT-SECRET', \
                      user_agent='YOUR-USER-AGENT', \
                      username='YOUR-REDDIT-USERNAME', \
                      password='YOUR-REDDIT-PASSWORD')
