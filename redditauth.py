9965401fde6466198a0fc96b87925a04a5df8417
healspersecond <ckiene@uw.edu>
2019-09-28 10:27:33 -0700

    Create redditauth.py

---
+++ redditauth.py
@@ -0,0 +1,10 @@
+import praw
+
+
+reddit = praw.Reddit(client_id='YOUR-ID', \
+                     client_secret='YOUR-SECRET', \
+                     user_agent='YOUR-USER-AGENT', \
+                     username='YOUR-REDDIT-USERNAME', \
+                     password='YOUR-REDDIT-PASSWORD')
+
+
