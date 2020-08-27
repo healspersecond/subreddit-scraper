aff171d748738e04ef4c58209c9cb56ddc8a9f72
healspersecond <ckiene@uw.edu>
2020-08-27 13:39:32 -0700

    Updated with better comments.

--- subscraper.py
+++ subscraper.py
@@ -10,16 +10,16 @@ print(time.time())
 # Reads in and execute the source of the redditauth.py file.
 exec(open("redditauth.py").read())
 
-# Enter the path with the list you want the scraper to read from. This is using the txt file of randomized subreddit names used in the study.
-inputs = 'top250subs.txt'
+# Enter the path to the subreddit list. Make sure the subreddit names don't contain "/r/" (e.g., Politics instead of /r/Politics)
+subreddit_names = 'top250subs.txt'
 
 # Enter the path you want your scraper to store the data.
-outputs = 'output.csv'
+csvfile = 'subreddit_data.csv'
 
-# Below is the loop that will read from each line of the inputs file and scrape the subreddit for that line.
+# Read in each string from the subreddit list through this loop and write out to the csv.
 
-with open(inputs,'r') as intr:
-    with open(outputs, 'w') as outr:
+with open(subreddit_names,'r') as sub_names_list:
+    with open(csvfile, 'w') as csv_file:
  # Data frame structure       
         sub_dict_empty = { "input_name": [],
                             "status": [],
@@ -33,43 +33,41 @@ with open(inputs,'r') as intr:
                             "mod_count": [],
                             "mod_list": [],
                             "advertiser_cat":[]}
-# Note: You can add or remove these parameters depending on what you want to include in your scrape. 
-# Refer to the PRAW documentation and the Reddit API references / documentation for how to get the data you want.
+# These aren't the only things you can get from PRAW, so check out its documentation if this isn't what you're looking for.
 
-        pd.DataFrame(sub_dict_empty).to_csv(outputs, mode="w", header=True, index=False)
+        pd.DataFrame(sub_dict_empty).to_csv(csvfile, mode="w", header=True, index=False)
 
-        for line in intr:
-            line = line.rstrip()
-            if line == "":
+        for name in sub_names_list:
+            name = name.rstrip()
+            if name == "":
                 break
 
-            print("starting: ", line)
+            print("starting: ", name)
 
             sub_dict = sub_dict_empty.copy()
-            sub_dict["input_name"] = [line]
+            sub_dict["input_name"] = [name]
 
             try:
-                # Variable to store the subreddit name from each line in the text file
-                linesub = reddit.subreddit(line) 
+                # Variable to store the subreddit name from each string in the sub_names_list list.
+                subreddit = reddit.subreddit(name) 
                 # Variable to store the ListingGenerator from prawcore for moderators needed for this study.
-                mods = [m for m in linesub.moderator()]
-                mod_list = ",".join([m.id for m in linesub.moderator()])
+                mods = [m for m in subreddit.moderator()]
+                mod_list = ",".join([m.id for m in subreddit.moderator()])
                 mod_count =  len(mods)
         
- # Data frame column functions
-                sub_dict["display_name"] = [line]
-                sub_dict["id"] = [linesub.id]
-                sub_dict["created"] = [linesub.created]
-                sub_dict["description_html"] = [linesub.description_html]
-                #sub_dict["praw_pickle"] = pickle.dumps(linesub)
-                sub_dict["subscribers"] = [linesub.subscribers]
-                sub_dict["accounts_active"] = [linesub.accounts_active]
-                sub_dict["active_user_count"] = [linesub.active_user_count]
+ # The dictionary to use in building our data frame
+                sub_dict["display_name"] = [name]
+                sub_dict["id"] = [subreddit.id]
+                sub_dict["created"] = [subreddit.created]
+                sub_dict["description_html"] = [subreddit.description_html]
+                sub_dict["subscribers"] = [subreddit.subscribers]
+                sub_dict["accounts_active"] = [subreddit.accounts_active]
+                sub_dict["active_user_count"] = [subreddit.active_user_count]
                 sub_dict["mod_count"] = mod_count
                 sub_dict["mod_list"] = mod_list
-                sub_dict["advertiser_cat"] = [linesub.advertiser_category]
+                sub_dict["advertiser_cat"] = [subreddit.advertiser_category]
 
-# This line and the following code will prevent the script from breaking if your scrape involves subreddits that don't exist.
+# Catch exceptions that might break the script, like when the subreddit can't be accessed through the API.
             except prawcore.PrawcoreException as err: 
                 sub_dict["status"] = [str(err)]
                 sub_dict["display_name"] = [None]
@@ -84,9 +82,9 @@ with open(inputs,'r') as intr:
                 sub_dict["advertiser_cat"] = [None]
             else:
                 sub_dict["status"] = ["OK"]
-# This stores the scraped data into a data frame using Python.
+# Write out to csv with Pandas.DatFrame.to_csv.
             reds = pd.DataFrame(sub_dict)
-            reds.to_csv(outputs, index=False, header=False, mode='a')
+            reds.to_csv(csvfile, index=False, header=False, mode='a')
             
             time.sleep(1)
 print(time.time())
